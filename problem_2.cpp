556ea4f985c1b5770a9a9999c7e51dcb566e7165
Abdullaeva Sevara <112007863+SevaraAbd@users.noreply.github.com>
2022-09-26 19:10:04 +0500

    Create problem_2.cpp

---
+++ problem_2.cpp
@@ -0,0 +1,7 @@
+/* Write C++ program that takes the radius(float) of the circle from user and calculates diameter,
+circumference and area of it. Print the result of the problem. */
+
+//ID:
+//Full Name:
+
+//Write your code here
