e97850f8c233beed4da214e1386932e221a94ddd
Abdullaeva Sevara <112007863+SevaraAbd@users.noreply.github.com>
2022-09-26 19:13:22 +0500

    Create problem_5.cpp

---
+++ problem_5.cpp
@@ -0,0 +1,8 @@
+/*Write C++ program that takes two numbers(int) from the user and checks whether the first
+one is greater or equal to the second. If the first is greater, it should print 1; if it is smaller, it
+should print 0*/
+
+//ID:
+//Full Name:
+
+//Write your code here
