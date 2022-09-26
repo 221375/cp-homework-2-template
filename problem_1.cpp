6c0c0ec9150b323eab36c68baa6ff936af4825d1
Abdullaeva Sevara <112007863+SevaraAbd@users.noreply.github.com>
2022-09-26 19:09:09 +0500

    Create problem_1.cpp

---
+++ problem_1.cpp
@@ -0,0 +1,7 @@
+/* Write C++ program that takes two numbers(int) from user and calculates sum and product of
+them. Print the result of summation and multiplication operations */
+
+//ID:
+//Full Name:
+
+//Write your code here
